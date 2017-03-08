9b0a8e553138c4e8dba13cbc870d27a5b069c55e
INDYashish <a.mishra@indy-cloud.com>
2017-03-08 11:46:55 +0700

    comment event.preventDefault

---
+++ package.js
@@ -0,0 +1,18 @@
+/**
+ * Created by indy-Ashish on 3/8/17.
+ */
+Package.describe({
+	summary: "A jQuery plugin for panning and zooming elements using CSS3",
+	version: '1.0.1',
+	git: "http://github.com/hitchcott/meteor-panzoom",
+	name: 'hitchcott:panzoom'
+});
+
+Package.on_use(function (api) {
+	if(api.versionsFrom) {
+		api.versionsFrom('METEOR@0.9.0');
+	}
+	api.use('jquery','client');
+
+	api.add_files('lib/dist/jquery.panzoom.js', ['client']);
+});
